b74d197c774e4a52dbbe68b45cd3258a72c3fe66
lucas alberto erazo muñoz <142178943+lukipuki0@users.noreply.github.com>
2023-10-17 13:34:11 -0300

    1

--- BlockBreaker/desktop/src/com/mygdx/game/DesktopLauncher.java
+++ BlockBreaker/desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher{
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("BlockBreaker");
+
 		new Lwjgl3Application(new BlockBreakerGame(), config);
 	}
 }
